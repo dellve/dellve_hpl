98100e9c005339e1d52562413d82dfc0c91d84f5
Konstantyn Komarov <komarov.konstant@utexas.edu>
2017-04-09 19:23:40 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup (
 	name='dellve_hpl',
 	version='0.0.1',
 	packages=find_packages(),
-	install_requires=['dellve'],
+	install_requires=['dellve', 'psutil'],
 	entry_points='''
 	[dellve.benchmarks]
 	HPL=dellve_hpl:HPL
